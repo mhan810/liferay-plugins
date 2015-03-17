b9bcfbbc5823d1b29f72f92fefac13b021c9c20e
Ethan Bustad <ethan.bustad@liferay.com>
2015-03-17 10:50:17 -0700

    LPS-54074 Source formatting

--- webs/kaleo-web/docroot/WEB-INF/src/com/liferay/portal/workflow/kaleo/runtime/notification/NotificationUtil.java
+++ webs/kaleo-web/docroot/WEB-INF/src/com/liferay/portal/workflow/kaleo/runtime/notification/NotificationUtil.java
@@ -65,7 +65,6 @@ public class NotificationUtil {
 		String notificationSubject = kaleoNotification.getDescription();
 
 		if (Validator.isNotNull(notificationSubject)) {
-
 			notificationSubject =
 				notificationMessageGenerator.generateMessage(
 					kaleoNotification.getKaleoClassName(),
